30cd766a235f6b311c2cf6d1741516f244f99c67
SkalskiP <piotr.skalski92@gmail.com>
2019-10-22 20:17:48 +0200

    Initial Commit

---
+++ HelloWorldApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorldApp
+//
+//  Created by Piotr Skalski on 22/10/2019.
+//  Copyright © 2019 Piotr Skalski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
